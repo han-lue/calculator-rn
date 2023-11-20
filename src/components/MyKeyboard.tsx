0eb53a8c041504f057dd4f02dbef1cb75346bcb5
username <username@usernames-mbp.home>
2023-11-20 11:12:29 +0100

    all buttons function as intended. frontend needs work

--- src/components/MyKeyboard.tsx
+++ src/components/MyKeyboard.tsx
@@ -9,14 +9,47 @@ export default function MyKeyboard() {
     const [firstNumber, setFirstNumber] = React.useState("");
     const [secondNumber, setSecondNumber] = React.useState("");
     const [operation, setOperation] = React.useState("");
-    const [result, setResult] = React.useState<Number | null >(null);
+    const [result, setResult] = React.useState<any | null >(null);
 
+
+
+    const firstNumberDisplay = () => {
+      if (result !== null) {
+          return <Text style={+result < 99999 ? [Styles.screenFirstNumber, {color: myColors.result}] : [Styles.screenFirstNumber, {fontSize: 50, color: myColors.result}]}>{result?.toString()}</Text>; 
+      }
+
+      if (firstNumber && firstNumber.length < 6) {
+        return <Text style={Styles.screenFirstNumber}>{firstNumber}</Text>;
+      }
+
+      if (firstNumber === "") {
+        return <Text style={Styles.screenFirstNumber}>{"0"}</Text>;
+      }
+
+      if (firstNumber.length > 5 && firstNumber.length < 8) {
+        return (
+          <Text style={[Styles.screenFirstNumber, { fontSize: 70 }]}>
+            {firstNumber}
+          </Text>
+        );
+      }
+      
+      if (firstNumber.length > 7) {
+        return (
+          <Text style={[Styles.screenFirstNumber, { fontSize: 50 }]}>
+            {firstNumber}
+          </Text>
+        );
+      }
+    };
 
     const handleNumberPress = (buttonValue: string) => {
-        if (firstNumber.length < 10) {
-          setFirstNumber(firstNumber + buttonValue);
-        }
-      };
+
+      if (firstNumber.length < 10) {
+        setResult(null);
+        setFirstNumber(firstNumber + buttonValue);
+      }
+    };
 
 
     const handleOperationPress = (buttonValue: string) => {
@@ -24,6 +57,13 @@ export default function MyKeyboard() {
         setSecondNumber(firstNumber);
         setFirstNumber("");
     };
+
+    const handleOperationOnResult = (buttonValue: string) => {
+      setOperation(buttonValue);
+      setSecondNumber(result);
+      setFirstNumber("");
+      setResult(null);
+    }; 
     
     const clearAll = () => {
         setFirstNumber("");
@@ -32,52 +72,34 @@ export default function MyKeyboard() {
         setResult(null);
     };
 
-    const firstNumberDisplay = () => {
-        if (result !== null) {
-            return <Text style={+result < 99999 ? [Styles.screenFirstNumber, {color: myColors.result}] : [Styles.screenFirstNumber, {fontSize: 50, color: myColors.result}]}>{result?.toString()}</Text>; 
-        }
-        if (firstNumber && firstNumber.length < 6) {
-          return <Text style={Styles.screenFirstNumber}>{firstNumber}</Text>;
-        }
-        if (firstNumber === "") {
-          return <Text style={Styles.screenFirstNumber}>{"0"}</Text>;
-        }
-        if (firstNumber.length > 5 && firstNumber.length < 8) {
-          return (
-            <Text style={[Styles.screenFirstNumber, { fontSize: 70 }]}>
-              {firstNumber}
-            </Text>
-          );
-        }
-        if (firstNumber.length > 7) {
-          return (
-            <Text style={[Styles.screenFirstNumber, { fontSize: 50 }]}>
-              {firstNumber}
-            </Text>
-          );
-        }
-      };
-
 
     const getResult = () => {
         switch (operation) {
           case "+":
               clearAll();
-              setResult(parseInt(secondNumber) + parseInt(firstNumber));
+              setResult(parseFloat(secondNumber) + parseFloat(firstNumber));
               break;
           case "-":
               clearAll();
-              setResult(parseInt(secondNumber) - parseInt(firstNumber));
+              setResult(parseFloat(secondNumber) - parseFloat(firstNumber));
               break;
           case "*":
               clearAll();
-              setResult(parseInt(secondNumber) * parseInt(firstNumber));
+              setResult(parseFloat(secondNumber) * parseFloat(firstNumber));
               break;
           case "/":
               clearAll();
-              setResult(parseInt(secondNumber) / parseInt(firstNumber));
+              setResult(parseFloat(secondNumber) / parseFloat(firstNumber));
               break;
-          default:
+          case "+/-":
+              clearAll();
+              setResult(parseFloat(secondNumber) * -1);
+              break;
+          case "%":
+              clearAll();
+              setResult(parseFloat(secondNumber) / 100);
+              break;
+          default: 
               clearAll();
               setResult(0);
               break;
@@ -94,39 +116,84 @@ export default function MyKeyboard() {
             alignSelf: "center",
             }}
         >
-            <Text style={Styles.screenSecondNumber}>
-            {secondNumber}
-            <Text style={{ color: "purple", fontSize: 50, fontWeight: '500' }}>{operation}</Text>
+           {
+           <Text style={Styles.screenSecondNumber}> {secondNumber} 
+              <Text style={{ color: "purple", fontSize: 50, fontWeight: '500' }}>{operation}</Text>
             </Text>
+            
+            }
             {firstNumberDisplay()}
         </View>
 
         <View style={Styles.row}>
             <Button title="C" isGray onPress={clearAll} />
-            <Button title="+/-" isGray onPress={() => handleOperationPress("+/-")} />
-            <Button title="％" isGray onPress={() => handleOperationPress("％")} />
-            <Button title="÷" isPurple onPress={() => handleOperationPress("/")} />
+
+            <Button title="+/-" isGray onPress={() => {
+                if (firstNumber === "" && secondNumber === "" && result !== null){ 
+                 handleOperationOnResult("+/-"); 
+                } else {
+                  handleOperationPress("+/-")
+                }}}
+              />
+
+            <Button title="％" isGray 
+            
+              onPress={() => {
+                if (firstNumber === "" && secondNumber === "" && result !== null){ 
+                handleOperationOnResult("%"); 
+                } else {
+                  handleOperationPress("%")
+                }}}
+              />
+
+            <Button title="÷" isPurple 
+              onPress={() => {
+                if (firstNumber === "" && secondNumber === "" && result !== null){ 
+                 handleOperationOnResult("/"); 
+                } else {
+                  handleOperationPress("/")
+                }}} />
         </View>
 
         <View style={Styles.row}>
             <Button title="7" onPress={() => handleNumberPress("7")} />
             <Button title="8" onPress={() => handleNumberPress("8")} />
             <Button title="9" onPress={() => handleNumberPress("9")} />
-            <Button title="×" isPurple onPress={() => handleOperationPress("*")} />
+            
+            <Button title="×" isPurple 
+              onPress={() => {
+                if (firstNumber === "" && secondNumber === "" && result !== null){ 
+                handleOperationOnResult("*"); 
+                } else {
+                  handleOperationPress("*")
+                }}} />
         </View>
 
         <View style={Styles.row}>
             <Button title="4" onPress={() => handleNumberPress("4")} />
             <Button title="5" onPress={() => handleNumberPress("5")} />
             <Button title="6" onPress={() => handleNumberPress("6")} />
-            <Button title="-" isPurple onPress={() => handleOperationPress("-")} />
+            
+            <Button title="-" isPurple 
+              onPress={() => {
+                if (firstNumber === "" && secondNumber === "" && result !== null){ 
+                 handleOperationOnResult("-"); 
+                } else {
+                  handleOperationPress("-")
+                }}} />
         </View>
 
         <View style={Styles.row}>
             <Button title="1" onPress={() => handleNumberPress("1")} />
             <Button title="2" onPress={() => handleNumberPress("2")} />
             <Button title="3" onPress={() => handleNumberPress("3")} />
-            <Button title="+" isPurple onPress={() => handleOperationPress("+")} />
+            <Button title="+" isPurple 
+              onPress={() => {
+                if (firstNumber === "" && secondNumber === "" && result !== null){ 
+                 handleOperationOnResult("+"); 
+                } else {
+                  handleOperationPress("+")
+                }}} />
         </View>
 
         <View style={Styles.row}>
